0db52b839f005871493f62056ef398247f289bf0
wrightzachary <taylorzw96@gmail.com>
2021-07-08 13:37:43 -0500

    Get song by id function added

--- MusicLibrary/music/views.py
+++ MusicLibrary/music/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.http import Http404
 from .models import Song
 from .serializers import SongSerializer
 from rest_framework.views import APIView
@@ -7,7 +7,7 @@ from rest_framework import status
 
 
 class SongList(APIView):
-
+    # get all from song list
     def get(self, request):
         song = Song.objects.all()
         serializer = SongSerializer(song, many=True)
@@ -19,3 +19,17 @@ class SongList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SongDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            raise Http404
+
+    # get song by ID
+    def get(self, request, pk):
+        song = self.get_object(pk)
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
