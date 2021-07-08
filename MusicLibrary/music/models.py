6858e6bfbc28522a4b1ebfacd0cbeef297939f56
wrightzachary <taylorzw96@gmail.com>
2021-07-08 13:18:39 -0500

    Framework built for DJANGO REST API

---
+++ MusicLibrary/music/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    artist = models.CharField(max_length=50)
+    album = models.CharField(max_length=50)
+    release_date = models.DateTimeField()
